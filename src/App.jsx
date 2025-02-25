0c46d476fd39b4a289686472e2b95ee261766c0a
akashkumarsky <akash220701@gmail.com>
2025-02-25 17:02:04 +0530

    addin about and mywork section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import MyWork from './Components/MyWork/MyWork'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
+      <MyWork />
     </div>
   )
 }
