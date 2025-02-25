d0213ed2d741bdd5064baa249841ad65cf0cf9b4
akashkumarsky <akash220701@gmail.com>
2025-02-25 11:55:20 +0530

    add navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.jpg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img src={logo} alt='Docplanner Group' />
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">Connect With Me</div>
+    </div>
   )
 }
 
