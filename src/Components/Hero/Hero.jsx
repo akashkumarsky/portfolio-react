9b7a8e3afdca0ea464ebfe0a5346d8305ce253bc
akashkumarsky <akash220701@gmail.com>
2025-02-25 17:00:34 +0530

    add hero componets

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Hero.css'
+import profile_pic from '../../assets/profile_pic.jpg'
 
 const Hero = () => {
   return (
-    <div className='hero'>Hero</div>
+    <div className='hero'>
+        <img src={profile_pic} alt='Profile' />
+        <h1><span>Hi, I'm Akash kumar,</span> Full Stack Java Developer Based In Bengalore India.</h1>
+        <p>I specialize in building high-quality websites and applications. Get in touch with me.</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-resume">My Resume</div>
+        </div>
+    </div>
   )
 }
 
