5d6c680c121299aca57ac9bf104492b1430c3c8c
akashkumarsky <akash220701@gmail.com>
2025-02-26 10:10:19 +0530

    adding data

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,60 @@
+import demo from './demo.gif'
+
+export const mywork_data = [
+    {
+        w_id: 1,
+        w_title: 'E-Commerce Clothing Store',
+        w_description: 'A full-stack e-commerce clothing store built with React, Redux, Firebase, and Stripe API.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/e-commerce-Frontend-React-'
+    },
+    {
+        w_id: 2,
+        w_title: 'Job Portal',
+        w_description: 'A full-stack job portal built with React, Node.js, Express, and MongoDB.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/job-portal-backend'
+    },
+    {
+        w_id: 3,
+        w_title: 'Secure Registration',
+        w_description: 'A secure registration form built with springboot,spring security,spring data jpa and postgresql.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/Secure-Registration-Backend'
+    },
+    {
+        w_id: 4,
+        w_title: 'Hotel Rating System',
+        w_description: 'A hotel rating system built with using Microservices.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/Hotel-Rating-System'
+    },
+    {
+        w_id: 5,
+        w_title: 'My Portfolio using React',
+        w_description: 'A modern, responsive portfolio website built with React.js, showcasing my projects and skills with smooth animations and clean UI design.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/portfolio-react'
+    },
+    {
+        w_id: 6,
+        w_title: 'Grievance Management System',
+        w_description: 'A PHP-based system for managing complaints and grievances with features like user authentication, ticket tracking, and admin dashboard for efficient complaint resolution.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/gms'
+    },
+    {
+        w_id: 7,
+        w_title: 'Banking System',
+        w_description: 'A simple banking system built with Java and MySQL, featuring user authentication, account management, and transaction history.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/banking-System/tree/main/bank'
+    },
+    {
+        w_id: 8,
+        w_title: 'Techblog',
+        w_description: 'A dynamic blog platform built with Java Servlets and JSP, featuring user authentication, blog post creation/management, categories, and interactive comments system.',
+        w_img: demo,
+        w_github: 'https://github.com/akashkumarsky/Techblog'
+    }
+]
