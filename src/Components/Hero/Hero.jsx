d91cb6fc0695d7df6b52bdfc5d5ddbcd74220452
akashkumarsky <akash220701@gmail.com>
2025-08-31 12:16:58 +0530

    all set

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_pic from '../../assets/profile_pic.jpg'
+import resume from '../../assets/resume.pdf'
 
 const Hero = () => {
   return (
@@ -9,8 +10,12 @@ const Hero = () => {
         <h1><span>Hi, I'm Akash kumar,</span> Java Full Stack Developer Based In Bengalore India.</h1>
         <p>I specialize in building high-quality websites and applications. Get in touch with me.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect With Me</div>
-            <div className="hero-resume">My Resume</div>
+            <div className="hero-connect" onClick={() => {
+                document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+            }}>Connect With Me</div>
+            <button className="hero-resume" onClick={() => {
+                window.open(resume, '_blank');
+            }}>My Resume</button>
         </div>
     </div>
   )
